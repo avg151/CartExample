e279238601ef5797124c08cf7a84bb36fd72c50c
Vladimir <avg151@ya.ru>
2025-12-13 13:50:23 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
+include(":product_list")
+include(":product_detail")
+include(":cart")
+include(":model")
+include(":database")
+include(":common")
+include(":navigation")

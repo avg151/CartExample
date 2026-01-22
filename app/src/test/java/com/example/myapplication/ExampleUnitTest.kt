291ed4e8b6c64a071a9550838d686eea40412c2e
Vladimir <avg151@ya.ru>
2026-01-22 16:02:44 +0700

    Coroutine Test

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,8 +1,17 @@
 package com.example.myapplication
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +19,39 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testViewModel() = runTest(testDispatcher) {
+        println("1")
+
+       launch {
+           println("2")
+           delay(100)
+           println("3")
+       }
+        launch {
+            println("4")
+            delay(500)
+            println("5")
+        }
+
+        println("6")
+        delay(300)
+
+        println("7")
+
+        delay(100)
+        println("8")
     }
 }

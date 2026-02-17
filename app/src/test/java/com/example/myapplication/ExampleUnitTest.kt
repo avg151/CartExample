458bdc66673d5087a7eba5df1889b5b79f9e27dc
Vladimir <avg151@ya.ru>
2026-02-17 13:01:02 +0700

    Coroutine Test

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -32,14 +35,28 @@ class ExampleUnitTest {
     }
 
     @Test
-    fun testViewModel() = runTest(testDispatcher) {
+    fun testViewModel1() = runTest(testDispatcher) {
         println("1")
 
        launch {
            println("2")
-           delay(100)
+           delay(200)
            println("3")
        }
+
+        coroutineScope {
+            println("enter coroutineScope")
+            launch {
+                println("A")
+                delay(500)
+                println("B")
+            }
+
+            println("C")
+            delay(100)
+            println("exit coroutineScope")
+        }
+
         launch {
             println("4")
             delay(500)
@@ -53,5 +70,68 @@ class ExampleUnitTest {
 
         delay(100)
         println("8")
+
+        /*
+        1
+        enter coroutineScope
+        C
+        2
+        A
+        exit coroutineScope
+        3
+        B
+        6
+        4
+        7
+        8
+        5
+         */
     }
+
+    @Test
+    fun testViewModel2() = runTest(testDispatcher) {
+        println("1")
+
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
+        println("2")
+
+        scope.launch {
+            println("3")
+
+            launch {
+                println("A")
+            }
+
+            println("4")
+            coroutineScope {
+                println("5")
+                launch {
+                    println("6")
+                    delay(500)
+                    println("B")
+                }
+
+                println("7")
+                delay(100)
+                println("C")
+            }
+
+            println("D")
+        }.join()
+        /*
+            1
+            2
+            3
+            4
+            5
+            A
+            7
+            6
+            C
+            B
+            D
+         */
+
+}
 }
